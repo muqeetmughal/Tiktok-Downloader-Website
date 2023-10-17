b315c97bcf9f684e07a6563db91474563097cf79
Hamza <88616464+muhammadhamza2001@users.noreply.github.com>
2023-10-17 14:08:05 +0500

    Blog cards created

--- layouts/ContentLayout.jsx
+++ layouts/ContentLayout.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const ContentLayout = (props) => {
   return (
     <>
-      <div className="container">
+      {/* <div className="container"> */}
         {/* <div className="w-full h-full sm:max-w-screen-md sm:mx-auto"> */}
         {props.children}
         {/* </div> */}
-      </div>
+      {/* </div> */}
     </>
   );
 };
--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,7 +1,59 @@
 import React from "react";
+import Image from 'next/image';
+import image from "./blog.jpg";
 
 const BlogPage = () => {
-  return <div>BlogPage</div>;
+  return (
+    <div className="px-8 lg:px-20">
+      <div className="flex flex-wrap justify-center my-5">
+        <div className="w-full lg:w-1/3 card my-2 lg:my-2">
+          <div className="bg-white rounded-lg shadow-md m-2">
+            <div className="relative h-40 w-full">
+              <Image
+                src={image}
+                alt="Card Image"
+                layout="fill"
+              />
+            </div>
+            <div className="p-4">
+              <h2 className="text-xl font-semibold">Card 1</h2>
+              <p>This is the card displayed on small screens.</p>
+            </div>
+          </div>
+        </div>
+        <div className="w-full lg:w-1/3 card my-2 lg:my-2">
+          <div className="bg-white rounded-lg shadow-md m-2">
+            <div className="relative h-40 w-full">
+              <Image
+                src={image}
+                alt="Card Image"
+                layout="fill"
+              />
+            </div>
+            <div className="p-4">
+              <h2 className="text-xl font-semibold">Card 2</h2>
+              <p>This is the second card.</p>
+            </div>
+          </div>
+        </div>
+        <div className="w-full lg:w-1/3 card my-2 lg:my-2">
+          <div className="bg-white rounded-lg shadow-md m-2">
+            <div className="relative h-40 w-full">
+              <Image
+                src={image}
+                alt="Card Image"
+                layout="fill"
+              />
+            </div>
+            <div className="p-4">
+              <h2 className="text-xl font-semibold">Card 3</h2>
+              <p>This is the third card.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BlogPage;
