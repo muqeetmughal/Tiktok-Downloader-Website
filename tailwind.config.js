af382bbae59f857d1725cb7bf38826215ef9d491
muqeetmughal <muqeetmughal786@gmail.com>
2023-10-22 10:06:36 +0500

    minor fixes more

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+
+const { join } = require("path");
+
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    join(__dirname, "./app/**/*.{js,ts,jsx,tsx}"),
+    join(__dirname, "./pages/**/*.{js,ts,jsx,tsx}"),
+    join(__dirname, "./components/**/*.{js,ts,jsx,tsx}"),
   ],
+
   theme: {
     container: {
       center: true,
     },
     extend: {},
     container: {
-      padding: {
-        DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-        '2xl': '6rem',
-      },
+      // padding: {
+      //   DEFAULT: "1rem",
+      //   sm: "2rem",
+      //   lg: "4rem",
+      //   xl: "5rem",
+      //   "2xl": "6rem",
+      // },
     },
   },
   plugins: [require("daisyui")],
@@ -31,4 +35,4 @@ module.exports = {
     prefix: "",
     darkTheme: false,
   },
-}
+};
