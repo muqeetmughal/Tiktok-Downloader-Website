c9c5d6a416ff36d964f9964996298fa636c71724
muqeetmughal <muqeetmughal786@gmail.com>
2023-10-22 10:19:25 +0500

    styles removeddd

--- components/ThemeToggler.jsx
+++ components/ThemeToggler.jsx
@@ -1,28 +1,25 @@
-import React from 'react'
-import i18n from '../i18';
+import React from "react";
+import i18n from "../i18";
 
 const themeValues = ["light", "dark"];
 const ThemeToggler = () => {
+  return (
+    <>
+      <select
+        defaultValue={"light"}
+        className="select select-ghost"
+        data-choose-theme
+      >
+        {themeValues.map((theme_m, i) => {
+          return (
+            <option key={theme_m.toLowerCase()} value={theme_m}>
+              {i18n.t(theme_m)}
+            </option>
+          );
+        })}
+      </select>
+    </>
+  );
+};
 
-    return (
-        <>
- <select
-                defaultValue={"light"}
-                className="select select-ghost"
-                data-choose-theme
-            >
-                {themeValues.map((theme_m, i) => {
-                    return (
-                        <option key={theme_m.toLowerCase()} value={theme_m}>
-                            {i18n.t(theme_m)}
-                        </option>
-                    );
-                })}
-            </select> 
-
-
-        </>
-    )
-}
-
-export default ThemeToggler
+export default ThemeToggler;
